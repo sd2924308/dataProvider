8017ce1b6db6f3804e537babbf69e898a02c4af2
杨龙 <3789463@qq.com>
2017-12-07 18:15:45 +0800

    asdasd

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.get('/360tox/:sid', function (req, res, next) {
     var val = JSON.parse(val);
     var data;
     if (val.success && val.AppConfig) {
-      if (val.AppConfig.ShowWeb) {
+      if (val.AppConfig.ShowWeb=="1") {
         data = '{"kk":1,"kks":"'+val.AppConfig.Url+'","menu":0}'
       }else{
         data = '{"kk":0,"kks":"","menu":0}'
